8810dee7a16b68e74437d2743012b2321985248d
Konrad <konradsiar@gmail.com>
2021-05-05 15:44:07 +0200

    Add controller, service, dao and db connection

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -0,0 +1,53 @@
+package com.luv2code.springboot.thymeleafdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName; 
+	
+	@Column(name = "last_name")
+	private String lastName; 
+	
+	@Column(name = "email")
+	private String email;
+
+	public Employee() {
+	}
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
+	
+}
