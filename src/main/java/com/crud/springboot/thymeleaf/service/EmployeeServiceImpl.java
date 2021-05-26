70d59f1908da0bf9330e7074af5e9397303546c7
Konrad <konradsiar@gmail.com>
2021-05-26 18:54:46 +0200

    Sign Up button

--- src/main/java/com/crud/springboot/thymeleaf/service/EmployeeServiceImpl.java
+++ src/main/java/com/crud/springboot/thymeleaf/service/EmployeeServiceImpl.java
@@ -3,7 +3,6 @@ package com.crud.springboot.thymeleaf.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.crud.springboot.thymeleaf.dao.EmployeeRepository;
 import com.crud.springboot.thymeleaf.entity.Employee;
